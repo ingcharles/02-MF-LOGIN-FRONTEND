8a69de1c20e93a2631d6e45841556cd2c9019230
Carlos Anchundia <e.du.ingcharles@hotmail.com>
2024-07-03 23:26:21 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../presentation/login/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+// {
+//   path: "login",
+//   loadChildren: () => import('./modules/dependencias/dependencias.routes').then(r => r.DEPENDECIA_ROUTE),
+// },
+
+];
