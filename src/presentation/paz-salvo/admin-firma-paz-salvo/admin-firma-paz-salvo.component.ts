ac640e371a0f8c7774f2c7398f5b13e1a02c5586
Carlos Anchundia <carlos.anchundia@funcionjudicial.gob.ecgit config --global user.email carlos.anchundia@funcionjudicial.gob.ec>
2024-08-02 16:56:53 -0500

    cambios

--- src/presentation/paz-salvo/admin-firma-paz-salvo/admin-firma-paz-salvo.component.ts
+++ src/presentation/paz-salvo/admin-firma-paz-salvo/admin-firma-paz-salvo.component.ts
@@ -7,6 +7,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { jsPDF } from 'jspdf';
+interface Registro {
+  area: string;
+  persona: string;
+}
+
 @Component({
   selector: 'app-admin-firma-paz-salvo',
   templateUrl: './admin-firma-paz-salvo.component.html',
@@ -31,6 +36,8 @@ export class AdminFirmaPazSalvoComponent implements OnInit {
   displayedColumns: string[] = ['area', 'persona'];
   registros: any[] = [];
   dataSource: any;
+  groupedData!: MatTableDataSource<any>;
+  groupedData1: any[]=[];
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
@@ -44,8 +51,38 @@ export class AdminFirmaPazSalvoComponent implements OnInit {
     if (this.pazSalvoForm.valid) {
       this.registros.push(this.pazSalvoForm.value);
       this.dataSource = new MatTableDataSource(this.registros);
+      this.actualizarDatosAgrupados();
       this.pazSalvoForm.reset();
     }
+  }
+  actualizarDatosAgrupados() {
+    // const groupedData = this.registros.reduce((acc, registro) => {
+    //   if (!acc[registro.area]) {
+    //     acc[registro.area] = { isGroup: true, area: registro.area, personas: [] };
+    //   }
+    //   acc[registro.area].personas.push(registro.persona);
+    //   return acc;
+    // }, {} as Record<string, { isGroup: boolean; area: string; personas: string[] }>);
+
+    // const dataWithHeaders = [];
+    // for (const group of Object.values(groupedData)) {
+    //   dataWithHeaders.push(group);
+    // }
+
+    // this.groupedData = new MatTableDataSource(dataWithHeaders);
+    const groupedData = this.registros.reduce((acc, registro) => {
+      (acc[registro.area] = acc[registro.area] || []).push(registro);
+      return acc;
+    }, {});
+     const dataWithHeaders = [];
+    for (const [area, registros] of Object.entries(groupedData)) {
+      dataWithHeaders.push({ isGroup: true, area: area }); // Encabezado del grupo
+      dataWithHeaders.push(registros); // Registros del grupo
+
+    }
+     this.groupedData1 = dataWithHeaders;
+
+    this.groupedData = new MatTableDataSource(dataWithHeaders);
   }
 
   generarPDF() {
@@ -57,6 +94,15 @@ export class AdminFirmaPazSalvoComponent implements OnInit {
       (acc[registro.area] = acc[registro.area] || []).push(registro);
       return acc;
     }, {});
+
+    // const dataWithHeaders = [];
+    // for (const [area, registros] of Object.entries(groupedData)) {
+    //   dataWithHeaders.push({ isGroup: true, area: area }); // Encabezado del grupo
+    //   dataWithHeaders.push(registros); // Registros del grupo
+
+    // }
+
+    // this.groupedData = new MatTableDataSource(dataWithHeaders);
 
     let y = 10; // Posición Y inicial
     for (const area in groupedData) {
